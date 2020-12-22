17e8f381bacbe989be8e8fd74725a98769d490a4
Brumo <70117557+B-rum-o@users.noreply.github.com>
2020-12-23 00:00:59 +0100

    Add files via upload

---
+++ BDRP_Client.lua
@@ -0,0 +1,3 @@
+RegisterCommand("discord", function ()
+    TriggerEvent("chatMessage", "^8 [The name of your server]" .. " ^6 Discord: ^7 ^_Yours discord adress")
+end) --/discord
---
+++ fxmanifest.lua
@@ -0,0 +1,7 @@
+fx_version 'cerulean'
+games { 'rdr3', 'gta5' }
+
+author 'Brumo'
+version '1.0.0'
+
+client_script 'BDRP_Client.lua'
